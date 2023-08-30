db63e7f8ebbefce3f808ad75e806877c0a2c628a
Kateryna Bielokopytova <Katherina.frontend@gmail.com>
2023-08-30 23:59:52 +0200

    canvas-sketch

---
+++ sketches/sketch_01.js
@@ -0,0 +1,38 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 600, 600 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    const w = 60;
+    const h = 60;
+    const gap = 20;
+    let x, y;
+    for(let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++){
+
+            let x = 100 + (w + gap) * i;
+            let y = 100 + (h + gap) * j;
+
+            context.beginPath();
+            context.rect(x, y, w, h);
+            context.stroke();
+
+            if(Math.random() > 0.5) {
+                context.beginPath();
+                context.rect(x + 6, y + 6, w - 12, h - 12);
+                context.stroke();
+            }
+
+            
+        }
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
